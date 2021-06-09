5306536615eda3e03faae0074f9933e401df1be4
sharenshaji <20mp1589@rit.ac.in>
2021-06-09 08:55:47 +0530

    programs

---
+++ pgm3_oddeven.java
@@ -0,0 +1,16 @@
+class pgm3_oddeven
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the number :");
+        int n=sc.nextInt();
+        if(n % 2 == 0)
+        
+            System.out.println("The given number "+n+" is Even ");
+        
+        else
+            System.out.println("The given number "+n+" is Odd ");
+	    
+    }
+}
