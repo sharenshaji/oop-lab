1814b49bb9e2684566085e9cf63c75a7c1c263fe
sharenshaji <77477473+sharenshaji@users.noreply.github.com>
2021-08-15 14:31:09 +0530

    Add files via upload

---
+++ pgm9_methodover.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+class pgm9_methodover
+{
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        overload o=new overload();
+        int a;
+        double x,y;
+        System.out.println("Enter the length of square : ");
+        a=sc.nextInt();
+        o.area(a);
+        System.out.println("Enter the length and breadth of rectangle : ");
+        x=sc.nextDouble();
+        y=sc.nextDouble();
+        o.area(x,y);
+        System.out.println("Ether the radius of circle : ");
+        x=sc.nextDouble();
+        o.area(x);
+        
+        sc.close();
+    }
+}
